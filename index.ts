b8be640a7815971163ab91c281c4828f16a0b6b0
Andrew Aluev <justpsst@yandex.ru>
2020-02-12 16:30:50 +0500

    fix import

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { configureService, sendRequest } from './src/brokerUtil';
-import { IMessage } from './src/interfaces/message';
-
-export {
-  IMessage,
-  configureService,
-  sendRequest
-}
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { configureService, sendRequest } from './brokerUtil';
+import { IMessage } from './interfaces/message';
+
+export {
+  IMessage,
+  configureService,
+  sendRequest
+}
